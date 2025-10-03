38f5de0f488a8b0efd44bde3ebf58c5a6078085a
Nguyễn Thị Cẩm Ly B21DCCN506 <camlynamly2018@gmail.com>
2025-10-03 17:32:04 +0700

    Update Firebase configuration values

--- app/firebase.js
+++ app/firebase.js
@@ -5,12 +5,12 @@ import {getFirestore} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKR16aBaIME0fKjS4Mr6LsurA5Y5ZS9Oc",
-  authDomain: "app1-7ca57.firebaseapp.com",
+  apiKey: "AIzaSyCKR16aBaIME0fKjS4Mr6LsurA5Y5ZS8Oc",
+  authDomain: "app1-7ca56.firebaseapp.com",
   projectId: "app1-7ca57",
   storageBucket: "app1-7ca57.appspot.com",
-  messagingSenderId: "457793889775",
-  appId: "1:457793889775:web:f3bf8c2fb4b03263dd021a"
+  messagingSenderId: "457796889775",
+  appId: "1:457793889775:web:f3bf8c2fb4b03263dd051a"
 };
 
 
